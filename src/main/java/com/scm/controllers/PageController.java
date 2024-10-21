936e88f711a896e7d01818369a54dd9fd955f344
Varun Kashyap <varunkashyap1810@gmail.com>
2024-10-21 23:29:26 +0530

    Thymeleaf Template

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,6 +3,9 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -15,5 +18,19 @@ public class PageController {
         model.addAttribute("youtubeChannel", "Learn Code with Durgesh");
         return "home";
     }
+
+    // about route
+    @RequestMapping("/about")  
+    public String aboutPage(){
+        return "about";
+    }
+
+    //services
+    // about route
+    @RequestMapping("/services")  
+    public String servicesPageLoading(){
+        return "services";
+    }
+    
 
 }
